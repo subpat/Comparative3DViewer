692948471468b9f78581af135946d95eec40029f
Subhadeep Patra <subhadeep.official@gmail.com>
2021-05-10 13:42:09 +0200

    Minor Compilation Bug Fixes

--- KiwiNative/src/main/jni/Application.mk
+++ KiwiNative/src/main/jni/Application.mk
@@ -1,13 +1,13 @@
 APP_OPTIM := release
 APP_PLATFORM := android-21
-APP_STL := gnustl_static
+APP_STL := c++_static
 #APP_CFLAGS
 APP_CPPFLAGS += -frtti
 APP_CPPFLAGS += -fexceptions
 APP_CPPFLAGS += -DANDROID
 #Unsupported abis for VTK v8.2.0 rc2 := arm64-v8a mips64
-#APP_ABI := armeabi armeabi-v7a x86 x86_64 mips
 APP_ABI := arm64-v8a armeabi armeabi-v7a mips mips64 x86 x86_64
+#APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
 LOCAL_ARM_NEON := true
 #APP_MODULES := KiwiNative
 #NDK_TOOLCHAIN_VERSION := clang
--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_ABI := arm64-v8a armeabi armeabi-v7a mips mips64 x86 x86_64
+#APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
 APP_STL := gnustl_shared
 APP_CPPFLAGS := -frtti -fexceptions
 APP_PLATFORM := android-21
